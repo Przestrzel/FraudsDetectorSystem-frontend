1ca19cdf069152afbdadcee7d6d466714dd36dde
Przestrzel <marek.przestrzelski00@gmail.com>
2022-07-07 22:31:44 +0200

    Create store for user auth

---
+++ src/store/auth.slice.ts
@@ -0,0 +1,20 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { User } from 'types/auth.types';
+
+const initialState = {
+  user: null
+};
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    saveUser: (state, action: PayloadAction<User>) => {
+      return { ...state,
+        user: action.payload };
+    }
+  }
+});
+
+export const { saveUser } = authSlice.actions;
+export const authReducer = authSlice.reducer;
