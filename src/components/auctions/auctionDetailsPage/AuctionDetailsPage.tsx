eb57616596d126bfef0d2b3e2a2ee83a796a4be0
Przestrzel <marek.przestrzelski00@gmail.com>
2022-09-10 18:29:36 +0200

    Add offer modal button

--- src/components/auctions/auctionDetailsPage/AuctionDetailsPage.tsx
+++ src/components/auctions/auctionDetailsPage/AuctionDetailsPage.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Loader from 'components/common/loader/Loader';
 import { useParams } from 'react-router-dom';
 import { getAuction } from 'services/__mocked__/auctions.service';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 import { AuctionDetails } from 'types/auctions.types';
 import { ReactComponent as Flag } from 'assets/icons/flag.svg';
 
@@ -26,45 +27,50 @@ const AuctionDetailsPage = () => {
 
   return (
     <div className={ styles.auctionDetailsPage }>
-      { isLoading ? <Loader /> : auction &&
-        <div className={ styles.auction }>
-          <div className={ styles.flagBox }>
-            <Tooltip
-              placement='top'
-              title={ `This auction is ${ auction.redFlag ? 'suspicious' : 'trusted' }` }
-            >
-              <Flag className={ `${ styles.flag } ${ auction.redFlag ? styles.flagRed: '' }` } />
-            </Tooltip>
-          </div>
-          <div className={ styles.auctionInfo }>
-            <div>
-              <div className={ styles.auctionLabel }>Title</div>
-              <div className={ styles.auctionTitle }>{ auction.name }</div>
+      {
+        isLoading ? <Loader /> : auction &&
+          <div className={ styles.auction }>
+            <div className={ styles.flagBox }>
+              <Tooltip
+                placement='top'
+                title={ `This auction is ${ auction.redFlag ? 'suspicious' : 'trusted' }` }
+              >
+                <Flag className={ `${ styles.flag } ${ auction.redFlag ? styles.flagRed: '' }` } />
+              </Tooltip>
             </div>
-            <div>
-              <div className={ styles.auctionInfoDetail }>
-                <div className={ styles.auctionLabel }>Start date:</div>
-                <div>{ auction.startDate }</div>
+            <div className={ styles.auctionInfo }>
+              <div>
+                <div className={ styles.auctionLabel }>Title</div>
+                <div className={ styles.auctionTitle }>{ auction.name }</div>
               </div>
-              <div className={ styles.auctionInfoDetail }>
-                <div className={ styles.auctionLabel }>End date:</div>
-                <div>{ auction.endDate }</div>
-              </div>
-              <div className={ styles.auctionInfoDetail }>
-                <div className={ styles.auctionLabel }>Status:</div>
-                <div>{ auction.status }</div>
-              </div>
-              <div className={ styles.auctionInfoDetail }>
-                <div className={ styles.auctionLabel }>Criteria:</div>
-                <div>{ auction.criteria }</div>
+              <div>
+                <div className={ styles.auctionInfoDetail }>
+                  <div className={ styles.auctionLabel }>Start date:</div>
+                  <div>{ auction.startDate }</div>
+                </div>
+                <div className={ styles.auctionInfoDetail }>
+                  <div className={ styles.auctionLabel }>End date:</div>
+                  <div>{ auction.endDate }</div>
+                </div>
+                <div className={ styles.auctionInfoDetail }>
+                  <div className={ styles.auctionLabel }>Status:</div>
+                  <div>{ auction.status }</div>
+                </div>
+                <div className={ styles.auctionInfoDetail }>
+                  <div className={ styles.auctionLabel }>Criteria:</div>
+                  <div>{ auction.criteria }</div>
+                </div>
               </div>
             </div>
+            <div className={ styles.offers }>
+              <div className={ styles.auctionAddOffer }>
+                <div className={ styles.auctionLabel }>Offers</div>
+                <button className={ styles.addOffer }><AddCircleIcon />Add offer</button>
+              </div>
+              <AuctionOffers offers={ auction.offers } />
+            </div>
           </div>
-          <div className={ styles.offers }>
-            <div className={ styles.auctionLabel }>Offers</div>
-            <AuctionOffers offers={ auction.offers } />
-          </div>
-        </div> }
+      }
     </div>
   );
 };
